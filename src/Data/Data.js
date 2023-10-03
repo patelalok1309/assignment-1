666aa657c76dde73a39479eb7d1c21b49e2a40fa
patelalok1309 <alokdholu10@gmail.com>
2023-10-03 10:56:57 +0530

    added data file and home file

---
+++ src/Data/Data.js
@@ -0,0 +1,44 @@
+export const Data = [
+    {
+        id: 1,
+        name : 'Wagenor',
+        model : 2023,
+        price : 440,
+        milage : 6.1,
+    },
+    {
+        id: 2,
+        name : 'alto',
+        model : 2021,
+        price : 550,
+        milage : 3.1,
+    },
+    {
+        id: 3,
+        name : 'i20',
+        model : 2020,
+        price : 330,
+        milage : 4.2,
+    },
+    {
+        id: 4,
+        name : 'verna',
+        model : 2019,
+        price : 405,
+        milage : 7.0,
+    },
+    {
+        id: 5,
+        name : 'honda city ',
+        model : 2018,
+        price : 584,
+        milage : 5.7,
+    },
+    {
+        id:6,
+        name : 'maruti 800',
+        model : 2002,
+        price : 200,
+        milage : 9.7,
+    },
+]
